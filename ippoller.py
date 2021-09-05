fdf4016cdd89241707a3a433fabc9d6d4b8d4bc0
Jari Jarvi <jari.jarvi@gmail.com>
2021-09-05 21:13:35 +0300

    Poll for IP changes periodically

--- ippoller.py
+++ ippoller.py
@@ -1,12 +1,13 @@
 import http.client
 import re
+import time
 
 
 class DynDnsProvider:
 
     url = "checkip.dyndns.org"
 
-    def getIP(self):
+    def get_ip(self):
         conn = http.client.HTTPConnection(self.url)
         conn.request("GET", "/")
         maxlen = 512
@@ -17,11 +18,36 @@ class DynDnsProvider:
         return match.group(1) if match and len(match.groups()) == 1 else ''
 
 
-def main():
-    provider = DynDnsProvider()
-    print(provider.getIP())
 
+class IpPoller:
+
+    def __init__(self, provider, on_ip_changed_cb):
+        self.provider = provider
+        self.period_sec = 60 * 30
+        self.running = True
+        self.known_ip = None
+        self.on_ip_changed_cb = on_ip_changed_cb
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        self.running = True
+        while self.running:
+            ip = self.provider.get_ip()
+            self._check_if_ip_changed(ip)
+            time.sleep(self.period_sec)
+
+    def _check_if_ip_changed(self, ip):
+        if self.known_ip == None:
+            self.known_ip = ip
+        elif ip != self.known_ip:
+            self.on_ip_changed_cb(ip, self.known_ip)
+            self.known_ip = ip
+
+
+def on_ip_changed(new_ip, previous_ip):
+    print('IP changed from {} to {}'.format(previous_ip, new_ip))
 
 if __name__ == '__main__':
-    main()
-
+    IpPoller(DynDnsProvider(), on_ip_changed)
--- testippoller.py
+++ testippoller.py
@@ -1,7 +1,6 @@
 import unittest
 import unittest.mock
-from ippoller import DynDnsProvider
-
+from ippoller import DynDnsProvider, IpPoller
 
 class TestDynDnsResponse(unittest.TestCase):
 
@@ -16,8 +15,46 @@ class TestDynDnsResponse(unittest.TestCase):
     @unittest.mock.patch('http.client.HTTPConnection.getresponse', return_value=ResponseStub())
     def test_parsing(self, mock_getresponse, mock_request):
         provider = DynDnsProvider()
-        self.assertEqual("192.168.1.10", provider.getIP())
+        self.assertEqual("192.168.1.10", provider.get_ip())
         mock_request.assert_called_once_with('GET', '/')
         mock_getresponse.assert_called_once_with()
 
 
+class TestIpPoller(unittest.TestCase):
+
+    def setUp(self):
+        self.num_rounds = 0
+        self.num_rounds_to_run = 1
+        self.ip_to_return = '192.168.1.10'
+        self.on_ip_changed_cb = unittest.mock.MagicMock()
+        self.poller = IpPoller(self, self.on_ip_changed_cb)
+
+    def get_ip(self):
+        self.num_rounds += 1
+        if self.num_rounds == self.num_rounds_to_run:
+            self.poller.stop()
+            self.num_rounds = 0
+        return self.ip_to_return
+
+    @unittest.mock.patch('time.sleep')
+    def test_request_is_sent_periodically(self, mock_sleep):
+        expected_period_in_seconds = 60 * 30
+        self.num_rounds_to_run = 3
+        self.poller.run()
+        self.assertEqual([unittest.mock.call(expected_period_in_seconds),
+                          unittest.mock.call(expected_period_in_seconds),
+                          unittest.mock.call(expected_period_in_seconds)],
+                         mock_sleep.mock_calls)
+
+    @unittest.mock.patch('time.sleep')
+    def test_ip_change(self, _):
+        first_ip = '192.168.1.10'
+        second_ip = '192.168.1.20'
+        self.ip_to_return = first_ip
+        self.poller.run()
+        self.on_ip_changed_cb.assert_not_called()
+        self.poller.run()
+        self.on_ip_changed_cb.assert_not_called()
+        self.ip_to_return = second_ip
+        self.poller.run()
+        self.on_ip_changed_cb.assert_called_once_with(second_ip, first_ip)
